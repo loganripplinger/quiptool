4ef2723f66a1cfaa7dba25a4f86c11f6888c27a5
Logan Ripplinger <logan.ripplinger@gmail.com>
2018-09-05 12:40:21 -0700

    Add create comment. Add show all comments for quip.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,12 @@
 class CommentsController < ApplicationController
+	def create
+		@quip = Quip.find(params[:quip_id])
+		@comment = @quip.comments.create(comment_params)
+		redirect_to quip_path(@quip)
+	end
+
+	private
+		def comment_params
+			params.require(:comment).permit(:commenter, :body)
+		end
 end
