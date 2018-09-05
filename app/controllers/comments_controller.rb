bd0ce5da04dbc4b6db35024be0abde913c6d5bf1
Logan Ripplinger <logan.ripplinger@gmail.com>
2018-09-05 12:49:54 -0700

    Delete comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,13 @@ class CommentsController < ApplicationController
 		redirect_to quip_path(@quip)
 	end
 
+	def destroy
+		@quip = Quip.find(params[:quid_id])
+		@comment = @quip.comments.find(params[:id])
+		@comment.destroy
+		redirect_to quip_path(@quip)
+	end
+
 	private
 		def comment_params
 			params.require(:comment).permit(:commenter, :body)
