077f41aeda9986b79478be338ea00f4827f2f0ba
Logan Ripplinger <logan.ripplinger@gmail.com>
2018-09-14 14:38:02 -0700

    Finish blue styling. Add to all pages.

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,6 @@ Rails.application.routes.draw do
   	resources :diffs
   end
   
-  root 'quips#index'
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
